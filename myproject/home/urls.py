3cfb9d0b631ae6ab4fd57b9ade0c960104df6d8f
Ephraim.Sun <ephraim888sun@gmail.com>
2021-07-24 13:31:03 -0500

    Update

--- myproject/home/urls.py
+++ myproject/home/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
-    path('portfolio/', views.portfolio, name='portfolio')
+    path('portfolio/',  views.portfolio.as_view(), name='portfolio'),
+    path('portfolio/post/<int:pk>', views.PostDetail.as_view(), name='post_detail')
 ]
--- myproject/home/views.py
+++ myproject/home/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+from django.views import generic
+from .models import Post
 
 # Create your views here.
 def home(request):
@@ -10,5 +13,17 @@ def contact(request):
 def about(request):
     return render(request, 'home/about.html')
 
-def portfolio(request):
-    return render(request, 'home/portfolio.html')
+# def portfolio(request):
+#     return render(request, 'home/portfolio.html')
+
+class portfolio(generic.ListView):
+    model = Post
+    template_name = 'home/portfolio.html'
+
+# class PostList(generic.ListView):
+#     queryset = Post.objects.filter(status=1).order_by('-created_on')
+#     template_name = ''
+# #
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'home/post_detail.html'
